37c4b588a32f74e1bdee74e81e091c5d3dc43d26
btzdnsn <15651003518@163.com>
2018-07-08 18:43:55 +0800

    tests

--- blog/urls.py
+++ blog/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from blogpost import views as blogpostViews
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+    url(r'^$', blogpostViews.index, name='main'),
+    url(r'^blog/(?P<slug>[^\.]+).html', blogpostViews.view_post, name='view_blog_post'),
+    url(r'^admin/', include(admin.site.urls))
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blogpost/tests.py
+++ blogpost/tests.py
@@ -1,3 +1,23 @@
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from django.test import TestCase
 
+from blogpost.views import index, view_post
+
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
 # Create your tests here.
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        self.assertIn(b'<title>Welcome to my blog</title>', response.content)
+
+        
+class BlogpostTest(TestCase):
+    def test_blogpost_url_resolves_to_blog_post_view(self):
+        found = resolve('/blog/this_is_a_test.html')
+        self.assertEqual(found.func, view_post)
